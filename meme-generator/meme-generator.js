73b81cc9da75ad30e84fcee4088e7d17b8948c16
yatzy95 <51012560+yatzy95@users.noreply.github.com>
2020-05-10 10:09:21 -0700

    Initial upload

---
+++ meme-generator/meme-generator.js
@@ -0,0 +1,130 @@
+let topTextInput, bottomTextInput, topTextSizeInput, bottomTextSizeInput, imageInput, generateBtn, canvas, ctx;
+
+function generateMeme (img, topText, bottomText, topTextSize, bottomTextSize) {
+    let fontSize;
+
+    // Size canvas to image
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw main image
+    ctx.drawImage(img, 0, 0);
+
+    // Text style: white with black borders
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.textAlign = 'center';
+
+    // Top text font size
+    fontSize = canvas.width * topTextSize;
+    ctx.font = fontSize + 'px Impact';
+    ctx.lineWidth = fontSize / 20;
+
+    // Draw top text
+    ctx.textBaseline = 'top';
+    topText.split('\n').forEach(function (t, i) {
+        ctx.fillText(t, canvas.width / 2, i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, i * fontSize, canvas.width);
+    });
+
+    // Bottom text font size
+    fontSize = canvas.width * bottomTextSize;
+    ctx.font = fontSize + 'px Impact';
+    ctx.lineWidth = fontSize / 20;
+
+    // Draw bottom text
+    ctx.textBaseline = 'bottom';
+    bottomText.split('\n').reverse().forEach(function (t, i) { // .reverse() because it's drawing the bottom text from the bottom up
+        ctx.fillText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+        ctx.strokeText(t, canvas.width / 2, canvas.height - i * fontSize, canvas.width);
+    });
+}
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("mydiv"));
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV: 
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+// //Get canvas data
+// function putImage()
+// {
+//   var canvas1 = document.getElementById("canvasSignature");        
+//   if (canvas1.getContext) {
+//      var ctx = canvas1.getContext("2d");                
+//      var myImage = canvas1.toDataURL("image/png");      
+//   }
+//   var imageElement = document.getElementById("MyPix");  
+//   imageElement.src = myImage;                           
+
+// }  
+
+function init () {
+    // Initialize variables
+    topTextInput = document.getElementById('top-text');
+    bottomTextInput = document.getElementById('bottom-text');
+    topTextSizeInput = document.getElementById('top-text-size-input');
+    bottomTextSizeInput = document.getElementById('bottom-text-size-input');
+    imageInput = document.getElementById('image-input');
+    generateBtn = document.getElementById('generate-btn');
+    canvas = document.getElementById('meme-canvas');
+    
+    ctx = canvas.getContext('2d');
+
+    canvas.width = canvas.height = 0;
+
+    // Default/Demo text
+    topTextInput.value = bottomTextInput.value = 'Demo\nText';
+
+    // Generate button click listener
+    generateBtn.addEventListener('click', function () {
+        // Read image as DataURL using the FileReader API
+        let reader = new FileReader();
+        reader.onload = function () {
+            let img = new Image;
+            img.src = reader.result;
+            generateMeme(img, topTextInput.value, bottomTextInput.value, topTextSizeInput.value, bottomTextSizeInput.value);
+        };
+        reader.readAsDataURL(imageInput.files[0]);
+    });
+}
+
+init();
